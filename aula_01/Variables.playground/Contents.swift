66f7fdcd378b40ef0c5c9e00f2bd600a69096d46
Melquias Ribeiro <melquias10.mr@gmail.com>
2022-03-08 17:24:00 -0300

    new: variables  class

---
+++ aula_01/Variables.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var multable = "rosa"
+let imutable = "Ribeiro"
+multable = "Melquias"
+//integer
+let age = 22
+
+var multLineString = """
+The best laid schemes
+O’ mice and men
+Gang aft agley
+"""
+
+
+print(multable)
+print(imutable)
+
+print(multable + imutable)
+print("\(multable) \(imutable),tem \(age)")
+print(multLineString)
